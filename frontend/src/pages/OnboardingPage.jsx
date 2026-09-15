4686233b1034897cb7ef47f4b68df035f0b15a65
vivek123234 <shahv8374@gmail.com>
2026-09-15 16:38:19 +0530

    Change random avatar URL to use loremfaces.net

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -39,7 +39,7 @@ const OnboardingPage = () => {
 
   const handleRandomAvatar = () => {
     const idx = Math.floor(Math.random() * 100) + 1; // 1-100 included
-    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+    const randomAvatar = `https://www.loremfaces.net/256/id/${idx}.jpg`;
 
     setFormState({ ...formState, profilePic: randomAvatar });
     toast.success("Random profile picture generated!");
